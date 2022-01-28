51e3c1fb82e735eeb66b04e3ae0626a76e50fc3b
raj vaya <rajkumarvaya@gmail.com>
2022-01-28 17:24:55 +0530

    Initial Commit

---
+++ loremipsum/loremipsumApp.swift
@@ -0,0 +1,17 @@
+//
+//  loremipsumApp.swift
+//  loremipsum
+//
+//  Created by raj vaya on 28/01/22.
+//
+
+import SwiftUI
+
+@main
+struct loremipsumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
